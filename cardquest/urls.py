4cfa51aacc9238d1e3ada11ac218b112a8d1d1b5
PSU-Maica <202280193@psu.palawan.edu.ph>
2023-12-01 20:44:39 +0800

    Added models and path

--- cardquest/urls.py
+++ cardquest/urls.py
@@ -1,10 +1,10 @@
 
 from django.urls import path
-from .views import HomePageView, TrainerList
+from .views import HomePageView, TrainerList, PokemonCardList, CollectionList
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('trainer_list', TrainerList.as_view(), name='trainer_list'),
-    path('pokemon-card', TrainerList.as_view(), name='pokemon-card'),
-    path('collection', TrainerList.as_view(), name='collection'),
+    path('pokemon-card', PokemonCardList.as_view(), name='pokemon-card'),
+    path('collection', CollectionList.as_view(), name='collection'),
 ]
--- cardquest/views.py
+++ cardquest/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from .models import PokemonCard, Trainer
+from .models import PokemonCard, Trainer, Collection
 
 
 class HomePageView(ListView):
@@ -12,8 +12,23 @@ class HomePageView(ListView):
         context = super().get_context_data(**kwargs)
         return context
     
+    
 class TrainerList(ListView):
     model = Trainer
     context_object_name = 'trainer'
     template_name = 'trainers.html'
     paginate_by = 15
+    
+    
+class PokemonCardList(ListView):
+    model = PokemonCard
+    context_object_name = 'pokemon-card'
+    template_name = 'pokemon-card.html'
+    paginate_by = 15
+    
+    
+class CollectionList(ListView):
+    model = Collection
+    context_object_name = 'collection'
+    template_name = 'collection.html'
+    paginate_by = 15    
