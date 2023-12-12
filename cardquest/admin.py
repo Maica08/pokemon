8527f50ec1419b32b703ec2b61cf1c22c81a507a
PSU-Maica <202280193@psu.palawan.edu.ph>
2023-12-12 14:20:53 +0800

    added remaining fields

--- cardquest/admin.py
+++ cardquest/admin.py
@@ -9,10 +9,16 @@ class TrainerAdmin(admin.ModelAdmin):
 
 @admin.register(PokemonCard)
 class PokemonAdmin(admin.ModelAdmin):
-    list_display = ('name', 'rarity', 'created_at', 'updated_at')
-    search_fields = ('name','rarity')
+    list_display = ('name', 'rarity','hp', 'card_type',
+                    'attack', 'description', 'weakness',
+                    'card_number', 'release_date', 'evolution_stage', 
+                    'abilities', 'created_at', 'updated_at')
+    search_fields = ('name','rarity','hp', 'card_type',
+                    'attack', 'description', 'weakness',
+                    'card_number', 'release_date', 'evolution_stage', 
+                    'abilities')
     
 @admin.register(Collection)
 class CollectionAdmin(admin.ModelAdmin):
-    list_display = ('trainer', 'card', 'created_at', 'updated_at')
+    list_display = ('trainer', 'card', 'collection_date', 'created_at', 'updated_at')
     search_fields = ('trainer', 'card')
